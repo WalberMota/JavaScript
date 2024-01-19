9bcc4310b338082ce856c8a14311b029e131deaa
WalberMota <mota.csharp@gmail.com>
2024-01-19 14:23:44 -0300

    particularidades - scripts.js

--- Particularidades/js/scripts.js
+++ Particularidades/js/scripts.js
@@ -6,3 +6,11 @@ function showAlert() {
     var myText = "Esta Função showAlert() está\nimplementada no arquivo scripts2.js\nchamado no final do HTML body";
     alert(myText);
 };
+
+// comentários de uma linha no js
+
+/*
+comentário
+de
+multiplas linhas no js
+ */
