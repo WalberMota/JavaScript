594ea540f4f07ef39d6dd84147d798e8d3d02bcd
WalberMota <mota.csharp@gmail.com>
2024-01-28 23:01:52 -0300

    estudos com variáveis js

--- Variáveis/js/scripts.js
+++ Variáveis/js/scripts.js
@@ -1,3 +1,10 @@
+/**
+ * No JavaScript é permitido criar variáveis de 3 formas
+ * 
+ * Podemos mudar o tipo da variável livremente (o js tem tipagem fraca)
+ * 
+ */
+
 var teste = 1;
 
 console.log(teste);
