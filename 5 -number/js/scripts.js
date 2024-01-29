783f63c0abe3a844ff936929c8269d5b527ea707
WalberMota <mota.csharp@gmail.com>
2024-01-29 16:39:17 -0300

    update number script.js

--- 5 -number/js/scripts.js
+++ 5 -number/js/scripts.js
@@ -8,7 +8,7 @@ var flutuante=5.32332322;
 console.log(flutuante);
 console.log(typeof flutuante); //vai imprimir 'number' pois tudo numero em js é um 'number'.
 
-var textoComNumero='texto com numero 5';
+var textoComNumero='texto com numero 5.323232';
 
 console.log(textoComNumero);
 console.log(typeof textoComNumero);//vai dizer que é tipo string
