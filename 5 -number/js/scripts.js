e826e23868ab030842e4b5db9eeb4540e732192c
WalberMota <mota.csharp@gmail.com>
2024-01-29 16:36:10 -0300

    update number script.js

--- 5 -number/js/scripts.js
+++ 5 -number/js/scripts.js
@@ -1,9 +1,9 @@
-/* var numero=5;
+var numero=5;
 
 console.log(numero);
 console.log(typeof numero);
- */
-var float=5.32332322;
 
-console.log(float);
-console.log(typeof float);
+var flutuante=5.32332322;
+
+console.log(flutuante);
+console.log(typeof flutuante); //vai imprimir number pois tudo numero em js é um number.
