60001adf0dde2f96b1fea2be751f8f89f4608221
WalberMota <mota.csharp@gmail.com>
2024-01-29 17:14:18 -0300

    update boolean script

---
+++ 7-boolean/js/scripts.js
@@ -0,0 +1,12 @@
+var verdade=true;
+
+console.log (verdade);
+console.log('tipo de variável = '+typeof verdade);
+document.write (`verdade=${verdade}</p>tipo de variável = ${typeof verdade}</p>`);//formato de template
+
+var verdade=false;
+
+console.log (verdade);
+console.log('tipo de variável = '+typeof verdade);
+
+document.write (`verdade=${verdade}</p>tipo de variável = ${typeof verdade}</p>`);
