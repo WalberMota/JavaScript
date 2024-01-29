a7ab86960fbad18f06afbc17165099d9273985be
WalberMota <mota.csharp@gmail.com>
2024-01-28 23:07:31 -0300

    estudos com variáveis js

--- Variáveis/js/scripts.js
+++ Variáveis/js/scripts.js
@@ -5,6 +5,8 @@
  * 
  * únicos caracteres especiais para iniciar variáveis são o $ e _ (undeline)
  * variávei não podem ser declaradas começando com um número.
+ * é de boa prática em casos de nomes compostos usar camelCase.
+ * ex.: var meuPrimeiroNome = "josé"
  */
 
 var teste = 1; //procurar sempre usar a palavra reservada var - ciando assim variáveis globais
